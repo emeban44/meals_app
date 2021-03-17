2ae79ec12d972f597c6105ec496663bfc13feb79
emeban44 <emi.cazorla@hotmail.com>
2021-03-17 14:46:50 +0100

    delete displayed meal

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -36,59 +36,67 @@ class MealDetailScreen extends StatelessWidget {
     final mealId = ModalRoute.of(context).settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text("${selectedMeal.title}"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Image.network(
-                  selectedMeal.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      appBar: AppBar(
+        title: Text("${selectedMeal.title}"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
               ),
-              buildSectionTitle(context, 'Ingredients:'),
-              buildContainer(
-                ListView.builder(
-                  itemBuilder: (ctx, index) => Card(
-                    color: Theme.of(context).accentColor,
-                    child: Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 5,
-                          horizontal: 10,
-                        ),
-                        child: Text(
-                          selectedMeal.ingredients[index],
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
-                        )),
-                  ),
-                  itemCount: selectedMeal.ingredients.length,
-                ),
-              ),
-              buildSectionTitle(context, 'Steps:'),
-              buildContainer(
-                ListView.builder(
-                  itemBuilder: (ctx, index) => Column(
-                    children: [
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Text('# ${(index + 1)}'),
-                        ),
-                        title: Text(selectedMeal.steps[index]),
+            ),
+            buildSectionTitle(context, 'Ingredients:'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
                       ),
-                      Divider(),
-                    ],
-                  ),
-                  itemCount: selectedMeal.steps.length,
+                      child: Text(
+                        selectedMeal.ingredients[index],
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )),
                 ),
-              )
-            ],
-          ),
-        ));
+                itemCount: selectedMeal.ingredients.length,
+              ),
+            ),
+            buildSectionTitle(context, 'Steps:'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${(index + 1)}'),
+                      ),
+                      title: Text(selectedMeal.steps[index]),
+                    ),
+                    Divider(),
+                  ],
+                ),
+                itemCount: selectedMeal.steps.length,
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        backgroundColor: Colors.red,
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
+      ),
+    );
   }
 }
