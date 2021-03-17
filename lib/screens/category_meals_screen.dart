302baf6f50e5b8fe86d293e91e11c8cf9995c709
emeban44 <emi.cazorla@hotmail.com>
2021-03-17 16:36:08 +0100

    filtering meals working

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/meal_item.dart';
 
-import '../dummy_data.dart';
 import '../models/meal.dart';
 
 class CategoryMealsScreen extends StatefulWidget {
@@ -11,6 +10,10 @@ class CategoryMealsScreen extends StatefulWidget {
 //  CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   static const routeName = '/category-meals';
+
+  final List<Meal> availableMeals;
+
+  CategoryMealsScreen(this.availableMeals);
 
   @override
   _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
@@ -34,7 +37,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
           ModalRoute.of(context).settings.arguments as Map<String, String>;
       categoryTitle = routeArgs['title'];
       final categoryId = routeArgs['id'];
-      displayedMeals = DUMMY_MEALS.where((meal) {
+      displayedMeals = widget.availableMeals.where((meal) {
         return meal.categories.contains(categoryId);
       }).toList();
       _loadedInitData = true;
